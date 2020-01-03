384a74540c1b229f5aff6a5921d703f6785b9862
Neil-Solomon <NeilSolomon89@gmail.com>
2020-01-02 22:32:11 -0500

    Updates

---
+++ precache-manifest.be4898ba253e0ee13f9519bb12e2957d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a2312319b12433baa2c633cfebf0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5820c6646083370ad95",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dd15dd657a52c8b1bdfb",
+    "url": "/static/js/2.ff7595ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ff7595ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5820c6646083370ad95",
+    "url": "/static/js/main.c728c98d.chunk.js"
+  },
+  {
+    "revision": "d89f1d212bf31a6f4937",
+    "url": "/static/js/runtime-main.06824518.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
