289efd65a859be00feff7ca4347009e71ecb7602
Neil-Solomon <NeilSolomon89@gmail.com>
2020-01-02 23:28:09 -0500

    fixed manifest.json, redirected domain

--- home_page/src/App.js
+++ home_page/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>Welcome to NeilSolomon.net !</h1>
+      <p>This is my software engineering portfolio. Enjoy!</p>
+      <a href="https://neil-solomon.github.io/score_keeper" target="blank">
+        Game Night!
+      </a>
+      <br></br>
+      <a href="https://linkedin.com/in/Neil-Solomon" target="blank">
+        LinkedIn
+      </a>
+      <br></br>
+      <a href="https://github.com/neil-solomon" target="blank">
+        GitHub
+      </a>
+      <br></br>
     </div>
   );
 }
